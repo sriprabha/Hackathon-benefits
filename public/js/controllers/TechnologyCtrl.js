5f621bde14730d94d3febf00b6b00ac896bac136
Sriprabha Gopalan <sgopala@L-SB8X2BJG8W-M.local>
2015-08-29 01:52:58 -0700

    Updates to css

--- public/js/controllers/TechnologyCtrl.js
+++ public/js/controllers/TechnologyCtrl.js
@@ -1,4 +1,22 @@
-angular.module('TechnologyCtrl', []).controller('TechnologyController', function($scope) {
-
+angular.module('TechnologyCtrl', []).controller('TechnologyController', function($scope,$location, $http, Benefits) {
 	
+	$scope.companies = ["ABCD", "EFGH", "JKLM", "1231", "12128", "ABCSI"];
+	$scope.clicked = false;
+	$scope.moveTo = function (item) {
+		$location.path( "/"+item );
+	}
+	$scope.reset = function() {
+
+	}
+
+	Benefits.get()
+          .success(function(data) {
+            console.log(data);
+            var output = [];
+            for(val in data) {
+            	output.push(data.text);
+            }
+            $scope.benefits = output;
+            
+          });
 });
