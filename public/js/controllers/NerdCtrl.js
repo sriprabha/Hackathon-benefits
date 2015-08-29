6427c07cc23f729796006f99b9604852f54faaf0
Ashwini Bhagwat <ashwini.bhagwat@gmail.com>
2015-08-29 04:04:22 -0700

    Add chart for: Most searched benefits

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -1,28 +1,31 @@
 angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
 
-	$('#container').highcharts({
-	    chart: {
-	        type: 'column'
-	    },
-	    title: {
-	        text: 'Column chart with negative values'
-	    },
-	    xAxis: {
-	        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-	    },
-	    credits: {
-	        enabled: false
-	    },
-	    series: [{
-	        name: 'John',
-	        data: [5, 3, 4, 7, 2]
-	    }, {
-	        name: 'Jane',
-	        data: [2, -2, -3, 2, 1]
-	    }, {
-	        name: 'Joe',
-	        data: [3, 4, 4, -2, 5]
-	    }]
-	});
-
+$('#container').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Most searched Employee Benefits'
+        },
+        xAxis: {
+            categories: ['Maternity Leave', 'Health Insurance','Onsite Day care', 'Telecommuting', 'Paternity Leave']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Number of searches'
+            }
+        },
+        legend: {
+            reversed: true
+        },
+        plotOptions: {
+            series: {
+                stacking: 'normal'
+            }
+        },
+        series: [{
+            data: [90, 60, 55, 45, 40]
+        }]
+    });
 });
