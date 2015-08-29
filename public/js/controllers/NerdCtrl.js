3b48371548fa747d318a9a7b4abcb6ced6b845ec
Ashwini Bhagwat <ashwini.bhagwat@gmail.com>
2015-08-29 03:34:21 -0700

    Chart in the analytics page

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -1,5 +1,28 @@
 angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
 
-	
+	$('#container').highcharts({
+	    chart: {
+	        type: 'column'
+	    },
+	    title: {
+	        text: 'Column chart with negative values'
+	    },
+	    xAxis: {
+	        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+	    },
+	    credits: {
+	        enabled: false
+	    },
+	    series: [{
+	        name: 'John',
+	        data: [5, 3, 4, 7, 2]
+	    }, {
+	        name: 'Jane',
+	        data: [2, -2, -3, 2, 1]
+	    }, {
+	        name: 'Joe',
+	        data: [3, 4, 4, -2, 5]
+	    }]
+	});
 
 });
